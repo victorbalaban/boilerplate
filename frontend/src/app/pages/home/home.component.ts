66922e329f00d4958a1aec5fa8684f520b15da77
MARIA-LAPTOP\Maria <dates_maria@yahoo.com>
2018-05-02 19:15:44 +0300

    login

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -13,38 +13,51 @@ import { Routes, RouterModule } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  profesors: string [];
-  professors:Professor[];
-  students: string [];
+  profesors: Professor [];
+  students: Student [];
   constructor(private http: HttpClient, private router: Router) {
     this.http.get('http://localhost:3000/profesors').subscribe(data=>{
           console.log(data);
-          this.profesors = data as string [];
+          this.profesors = data as Professor [];
       },(err: HttpErrorResponse) => {
         console.log (err.message);
       })
       this.http.get('http://localhost:3000/students').subscribe(data=>{
           console.log(data);
-          this.students = data as string [];
+          this.students = data as Student [];
       },(err: HttpErrorResponse) => {
         console.log (err.message);
       })
 
    }
    onClickSubmit(data) {
-    this.professors.forEach(prof => {
+    this.profesors.forEach(prof => {
       if ((data.user.name === prof.name) && (data.user.password === prof.password)) {
         this.router.navigateByUrl("/pages/note");      
       }  
 
     });
- }
+ }  
+
+ onClickSubmitStudent(data) {
+  this.students.forEach(stud => {
+    if ((data.user.name === stud.name) && (data.user.password === stud.password)) {
+      this.router.navigateByUrl("/pages/catalog");      
+    }  
+
+  });
+}
 
   ngOnInit() {
   }
 
 }
 interface Professor{
+  name:String;
+  password:String;
+}
+
+interface Student{
   name:String;
   password:String;
 }
